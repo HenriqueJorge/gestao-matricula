d625156b9f787f1db3d12edfb792dfeba5eebc74
HenriqueJorge <henrique.jorge6414@gmail.com>
2026-09-16 16:30:35 -0300

    Adiciona base do projeto

---
+++ src/main/java/com/gestao/GestaoMatriculaApplication.java
@@ -0,0 +1,13 @@
+package com.gestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoMatriculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoMatriculaApplication.class, args);
+	}
+
+}
